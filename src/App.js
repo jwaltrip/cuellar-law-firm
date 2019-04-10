a275873a758d081564e7511316664b334ebb5a2b
Jake Waltrip <jake.waltrip@gmail.com>
2019-04-10 16:43:15 -0500

    added initial template pages for the 4 main pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+// import the 4 main pages
+import HomePage from "./components/HomePage/HomePage";
+import ProfilePage from "./components/ProfilePage/ProfilePage";
+import ServicesPage from "./components/ServicesPage/ServicesPage";
+import ContactUsPage from "./components/ContactUsPage/ContactUsPage";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+  
+          <Route path="/" exact component={HomePage} />
+          <Route path="/profile/" component={ProfilePage} />
+          <Route path="/services/" component={ServicesPage} />
+          <Route path="/contact/" component={ContactUsPage} />
+        
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/ContactUsPage/ContactUsPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ContactUsPage extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <h1>Contact Us Page</h1>
+          <hr />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ContactUsPage;
