0f8f5b3c829311b9aa387cab72d8e8683a19bcb5
Zeke <zgutier5@gmail.com>
2019-04-13 19:51:19 -0500

    home components not working right

--- src/components/Banners/TitleBanner.js
+++ src/components/Banners/TitleBanner.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 class TitleBanner extends Component {
   render() {
     return (
-        <div className="title">
+        <div className="title">x
             <div class="page-section content-full shadow-inside-top-bottom" style={{backgroundColor: `${this.props.color}`, backgroundPosition:"top center", backgroundRepeat:"no-repeat", backgroundSize:"cover;", backgroundAttachment:"scroll", width: "100%"}}>
           
                 <div class="section-mask" style= {{backgroundColor: "rgba(0, 0, 0, 0.6)"}}></div>
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./HomePage.css";
 
 class HomePage extends Component {
   render() {
@@ -8,6 +9,96 @@ class HomePage extends Component {
           <h1>Home Page</h1>
           <hr />
         </div>
+        <div className="container">
+            <div className="section-content">
+                <div className="textaligncenter">
+                    <h2 className="md-heading textaligncenter">ASSURING YOU GET THE JUSTICE YOU DESERVE</h2>
+                </div>
+            </div>
+        </div>
+        <div className="container">
+          <div className="section-content">
+            <div>
+              <h2 className="md-heading textaligncenter">Services Offered </h2>
+              <h5 className="md-heading textaligncenter">Skilled and Knowledgeable</h5>
+              <div className="clearfix" style={{height:"40px"}}></div>
+              <div className="row">
+                <div className="col-md-4">
+                    <div className="md-box-icon animate fadeInUp align-left advanced-icon" data-delay="250">
+                        <div className="md-icon style-circle fill"><i className="entypo-user" style={{borderColor:"#09d9e9", backgroundColor:"#09d9e9", color:"#ffffff"}} data-color="#ffffff" data-bg="#09d9e9" data-bg-hover="#09d9e9" data-color-hover="#ffffff"></i></div>
+                        <div className="box-text">
+                            <h4>Personal Injury</h4>
+                        </div>
+                    </div>
+                    <div className="clearfix"></div>
+                </div>
+                <div className="col-md-4">
+                    <div className="md-box-icon animate fadeInUp align-left advanced-icon" data-delay="250">
+                        <div className="md-icon style-circle fill"><i className="icon-legal" style={{borderColor:"#09d9e9", backgroundColor:"#09d9e9", color:"#ffffff"}} data-color="#ffffff" data-bg="#09d9e9" data-bg-hover="#09d9e9" data-color-hover="#ffffff"></i></div>
+                        <div className="box-text">
+                            <h4>Criminal</h4>
+                        </div>
+                    </div>
+                    <div className="clearfix"></div>
+                </div>
+                <div className="col-md-4">
+                    <div className="md-box-icon animate fadeInUp align-left advanced-icon" data-delay="500">
+                        <div className="md-icon style-circle fill"><i className="entypo-users" style={{borderColor:"#09d9e9", backgroundColor:"#09d9e9", color:"#ffffff"}} data-color="#ffffff" data-bg="#09d9e9" data-bg-hover="#09d9e9" data-color-hover="#ffffff"></i></div>
+                          <div className="box-text">
+                            <h4>Family</h4>
+                          </div>
+                        </div>
+                        <div className="clearfix"></div>
+                    </div>
+                </div>
+                <div className="clearfix" style={{height: "60px"}}></div>
+            </div>
+        </div>
+     </div>
+     <div className="container2">
+          <div className="section-mask" style={{backgroundColor:"rgba(44, 62, 80, 0.9)"}}></div>
+          <div className="section-content">
+              <div>
+                  <h2 className="md-heading textaligncenter" style={{color:"#ffffff", fontSize:"48px", lineHeight:"56px", fontWeight:"300"}}>Why choose Michaela D. Cuellar</h2>
+                  <div className="clearfix hidden-sm hidden-xs" style={{height:"40px"}}><div>
+                  <div className="row">
+                      <div className="col-md-6">
+                          <div className="clearfix" style={{height:"90px"}}></div>
+                          <div className="md-box-icon animate fadeInLeft align-right simple-icon" data-delay="250">
+                              <div className="md-icon style-normal"><i className="entypo-mobile" style={{color:"#ffffff;"}}></i></div>
+                              <div className="box-text">
+                                  <h4 style={{textAlign: "right"}}><span style={{color: "#ffffff"}}>Quick&nbsp;Response&nbsp;</span></h4>
+                                  <p style={{textAlign: "right"}}><span style={{color: "#ffffff"}}>Jail Release Phones are answered 24 hours a day.</span></p>
+                              </div>
+                          </div>
+                          <div className="clearfix"></div>
+                          <div className="clearfix" style={{height:"30px"}}></div>
+                          <div className="md-box-icon animate fadeInLeft align-right simple-icon" data-delay="750">
+                              <div className="md-icon style-normal"><i className="lineicon-banknote" style={{color:"#ffffff"}}></i></div>
+                              <div className="box-text">
+                                  <h4 style={{textAlign: "right"}}><span style={{color: "#ffffff"}}>Payment Plans</span></h4>
+                                  <p style={{textAlign: "right"}}><span style={{color: "#ffffff"}}>Installment payment plans available.</span></p>
+                              </div>
+                          </div>
+                          <div className="clearfix"></div>
+                          <div className="clearfix" style={{height:"30px"}}></div>
+                          <div className="md-box-icon animate fadeInLeft align-right simple-icon" data-delay="500">
+                              <div className="md-icon style-normal"><i className="lineicon-settings3" style={{color: "#ffffff"}}></i></div>
+                              <div className="box-text">
+                                  <h4 style={{textAlign: "right"}}><span style={{color: "#ffffff"}}>Bilingual</span></h4>
+                                  <p style={{textAlign: "right"}}><span style={{color: "#ffffff"}}>Se Habla Español.</span></p>
+                              </div>
+                          </div>
+                          <div className="clearfix"></div>
+                      </div>
+                      <div className="col-md-6 hidden-sm hidden-xs textaligncenter"><img src="https://www.michaelacuellar.com/wp-content/uploads/2014/06/gable1.png" class="md-single-image animate fadeInDownBig" data-delay="679" alt=""/>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      </div>
+      </div>
       </div>
     );
   }
