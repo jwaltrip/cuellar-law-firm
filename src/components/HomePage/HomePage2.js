b78edbecf0a742e13354245ea5eb16f3ad1410dd
Jake Waltrip <jake.waltrip@gmail.com>
2019-04-14 21:48:09 -0500

    added justice assured banner w/ auto-fit text for small screens

--- src/components/HomePage/HomePage2.js
+++ src/components/HomePage/HomePage2.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./HomePage.css";
+
 import MichaelaBanner from "../Banners/MichaelaBanner";
+import JusticeBanner from "../Banners/JusticeBanner";
 
 class HomePage2 extends React.Component {
   render() {
@@ -8,6 +10,7 @@ class HomePage2 extends React.Component {
       <div>
         {/* banner image of Michaela */}
         <MichaelaBanner />
+        <JusticeBanner />
       </div>
     );
   }
