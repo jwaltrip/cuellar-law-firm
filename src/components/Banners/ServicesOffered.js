2528c8a3555cb7528daa4354db662051b747fdcd
Jake Waltrip <jake.waltrip@gmail.com>
2019-04-29 19:08:20 -0500

    touched up styling

--- src/components/Banners/ServicesOffered.js
+++ src/components/Banners/ServicesOffered.js
@@ -23,14 +23,14 @@ const ServicesOffered = (props) => {
             <div className="col-md-3 mb-3">
               <div className="d-flex flex-row justify-content-center">
                 <BlueIcon iconClass="fas fa-home fa-2x"/>
-                <div className="services-icon-text">24hr Jail Release</div>
+                <div className="services-icon-text">Jail Release</div>
               </div>
             </div>
   
             <div className="col-md-3 mb-3">
               <div className="d-flex flex-row justify-content-center">
                 <BlueIcon iconClass="fas fa-gavel fa-2x"/>
-                <div className="services-icon-text">Criminal</div>
+                <div className="services-icon-text">Criminal Defense</div>
               </div>
             </div>
             
@@ -57,14 +57,14 @@ const ServicesOffered = (props) => {
             <div className="col-md-3 mb-3">
               <div className="d-flex flex-row justify-content-start">
                 <BlueIcon iconClass="fas fa-home fa-2x"/>
-                <div className="services-icon-text">24hr Jail Release</div>
+                <div className="services-icon-text">Jail Release</div>
               </div>
             </div>
   
             <div className="col-md-3 mb-3">
               <div className="d-flex flex-row justify-content-start">
                 <BlueIcon iconClass="fas fa-gavel fa-2x"/>
-                <div className="services-icon-text">Criminal</div>
+                <div className="services-icon-text">Criminal Defense</div>
               </div>
             </div>
             
