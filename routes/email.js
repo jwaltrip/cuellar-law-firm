a38233c84e631d5fbe6270c975356aeefd63b542
Jake Waltrip <jake.waltrip@gmail.com>
2019-05-06 16:11:08 -0500

    initially implemented the server

---
+++ routes/email.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const mail = require("../utils/sendEmail");
+
+/* GET home page. */
+router.post('/email/send', (req, res, next) => {
+  const { email, name, subject, message } = req.body;
+  
+  // return an error if any of these parameters are missing or empty
+  if  (!email || !name || !subject || !message) {
+    return res.json({
+      success: false,
+      error: "You must provide info for Email, Name, Subject and Message fields"
+    });
+  }
+  
+  // send the email
+  mail.sendEmail(email, name, subject, message)
+    .then(result => {
+      // log the result of sending the email and email details
+      console.log(result);
+      // return the email info as json
+      return res.json({ result });
+    })
+    .catch(console.error);
+});
+
+module.exports = router;
---
+++ utils/sendEmail.js
@@ -0,0 +1,90 @@
+const nodeMailer = require("nodemailer");
+const Email = require('email-templates');
+const moment = require("moment");
+
+// function to send email
+exports.sendEmail = function (recipient, name, subject, message) {
+  return new Promise((resolve, reject) => {
+    const email = new Email();
+    const locals = {
+      name: name,
+      date: moment().format("MMM D, YYYY @ LT"), // formats the current date like "May 5, 2019 @ 9:30pm"
+      subject: subject,
+      message: message,
+      email: recipient
+    };
+    
+    Promise
+      .all([
+        email.render('ContactForm/html', locals),
+        email.render('ContactForm/subject', locals),
+        email.render('ContactForm/text', locals),
+      ])
+      .then(([ html, subject, text ]) => {
+        console.log('html: ', html);
+        console.log('subject: ', subject);
+        console.log('text: ', text);
+        
+        let transporter = nodeMailer.createTransport({
+          host: 'smtp.googlemail.com', // Gmail Host
+          port: 465, // Port
+          secure: true, // this is true as port is 465
+          auth: {
+            user: process.env.CUELLAR_CONTACT_EMAIL_USER, //Gmail username
+            pass: process.env.CUELLAR_CONTACT_EMAIL_PASS // Gmail password
+          }
+        });
+        
+        // if the TEST_EMAIL env var is set to true, add "TEST MAIL" to the subject
+        const fromEmail = (process.env.TEST_EMAIL) ?
+                          `"Cuellar Law - TEST MAIL" <${process.env.CUELLAR_EMAIL}>` :
+                          `"Cuellar Law" <${process.env.CUELLAR_EMAIL}>`;
+        
+        let mailOptions = {
+          from: fromEmail,
+          to: recipient, // Recepient email address. Multiple emails can send separated by commas
+          replyTo: recipient,
+          subject: subject,
+          html: html,
+          text: text
+        };
+        
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.log(error);
+            // create object to return containing email information
+            const emailInfo = {
+              from: mailOptions.from,
+              to: mailOptions.to,
+              subject: mailOptions.subject,
+              html: html,
+              text: text,
+              info: info,
+              errors: error
+            };
+            
+            // fulfill the promise - essentially a return statement
+            // there was an error sending the email
+            resolve(emailInfo);
+          } else {
+            // else, no error. msg sent successfully
+            console.log('Message sent: %s', info.messageId);
+            // create object to return containing email information
+            const emailInfo = {
+              from: mailOptions.from,
+              to: mailOptions.to,
+              subject: mailOptions.subject,
+              html: html,
+              text: text,
+              info: info,
+              errors: null
+            };
+  
+            // fulfill the promise upon successful send of email
+            resolve(emailInfo);
+          }
+        });
+      })
+      .catch(console.error);
+  });
+};
